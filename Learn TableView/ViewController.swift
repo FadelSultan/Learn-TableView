b02683f91615c012c35b646c488662ca09732412
FadelSultan <srqa3h.com@gmail.com>
2018-12-09 20:01:07 +0300

    Initial Commit

---
+++ Learn TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learn TableView
+//
+//  Created by fadel sultan on 12/9/18.
+//  Copyright © 2018 fadel sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
